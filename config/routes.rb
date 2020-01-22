c3e3385cc88693ce3a8fcff07df0a5b733ea3bba
Yuki Ito <y.ito.works@gmail.com>
2020-01-22 11:40:43 +0900

    Create devise sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "static_pages#home"
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   get 'static_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
